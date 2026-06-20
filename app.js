4436da90d005a07f60e2e2626278b9b9fe06ce67
Emanuel <emanuelalbaitero@gmail.com>
2026-06-20 19:16:59 -0300

    se agrega un timeout a app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const { sum, res, mul } = require("./maths.js");
 
-console.log(sum(10, 11));
-
-console.log(res(15, 11));
-console.log(mul(10, 5));
+setTimeout(() => {
+  console.log(sum(2, 3));
+}, 3000);
