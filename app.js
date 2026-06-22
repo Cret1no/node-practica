60802366f87b945414fc2d918fd5d2bc4132c5c7
Emanuel <emanuelalbaitero@gmail.com>
2026-06-22 13:27:00 -0300

    Modulo FS(metodos readFile, rename, appendFile)

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-// se importa maths.js con require y destructuring
+// importamos el modulo built-in fs(file-system)
 
-const { sum, res, mul } = require("./maths.js");
-const { mostrarTema } = require("./mostrar-tema.js");
+const fs = require("fs");
 
-//setImmediate se ejecuta despues de todo el codigo sincrono
+//usamos el metodo readFile para leer archivo, sabemos que es un metodo asyn por lo que el tercer
+// parametro es una funcion para manejar la respuesta exitosa o fallida(Ej: err/data)
+// ubicacion del archivo que queremos leer | codificacion | funcion a ejecutar despues de la lectura
+//throw err detiene la funcion mientras que console.log(err) no
 
-setInterval(mostrarTema, 2000, "Node js");
+/*fs.readFile("./index.html", "utf-8", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data);
+});*/
+
+//cambiar nombre de una archivo con el metodo rename
+//archivo a cambiar nombre | nuevo nombre | funcion con (err)
+
+/*fs.rename("./index.html", "main.html", (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Se modifico el nombre del archivo correctamente");
+});*/
+
+//Agregar contenido al final de un archivo
+//archivo | contenido | funcion(err)
+
+fs.appendFile(
+  "./index.html",
+  "<p>Este parrafo es agregado al final con el metodo appendFile</p>",
+  (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("Archivo modificado exitosamente");
+  },
+);
