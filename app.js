1f0fb73769bb0e925bcf4ff57079bce5fb42cfc4
Emanuel <emanuelalbaitero@gmail.com>
2026-06-22 17:42:23 -0300

    Se agrega el SYNC a los metodos para ejecutarlos en orden

--- app.js
+++ app.js
@@ -2,57 +2,19 @@
 
 const fs = require("fs");
 
-//usamos el metodo readFile para leer archivo, sabemos que es un metodo asyn por lo que el tercer
-// parametro es una funcion para manejar la respuesta exitosa o fallida(Ej: err/data)
-// ubicacion del archivo que queremos leer | codificacion | funcion a ejecutar despues de la lectura
-//throw err detiene la funcion mientras que console.log(err) no
+//Vamos a usar los metodos en formato "SYNC" para que sean sincronos y se puedan ejecutar en orden
 
-/*fs.readFile("./index.html", "utf-8", (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log(data);
-});*/
+const data = fs.readFileSync("./index.html", "utf-8");
 
-//cambiar nombre de una archivo con el metodo rename
-//archivo a cambiar nombre | nuevo nombre | funcion con (err)
+console.log(data);
 
-/*fs.rename("./index.html", "main.html", (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("Se modifico el nombre del archivo correctamente");
-});*/
+fs.renameSync("./index.html", "main.html");
 
-//Agregar contenido al final de un archivo
-//archivo | contenido | funcion(err)
-
-/*fs.appendFile(
-  "./index.html",
+fs.appendFileSync(
+  "./main.html",
   "<p>Este parrafo es agregado al final con el metodo appendFile</p>",
-  (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Contenido agregado exitosamente");
-  },
-);*/
+);
 
-//modificar contenido de un archivo, si el archivo no existe, lo crea
-// archivo a modificar | nuevo contenido | manejo de error
-/*fs.writeFile("./main.html", "<h1>Este es el nuevo contenido</h1>", (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("Archivo modificado exitosamente");
-});*/
+fs.writeFileSync("./main.html", "<h1>Este es el nuevo contenido</h1>");
 
-//eliminar un archivo, metodo unlink
-// archivo | manejo de error
-
-/*fs.unlink("./main.html", (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("Archivo eliminado exitosamente");
-});*/
+fs.unlinkSync("./main.html");
