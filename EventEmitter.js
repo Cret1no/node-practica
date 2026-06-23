21261927451f0bb03b47da33f23868d3ef66af8a
Emanuel <emanuelalbaitero@gmail.com>
2026-06-23 18:34:14 -0300

    Pequeño repaso de promesas

--- EventEmitter.js
+++ EventEmitter.js
@@ -1,14 +1,16 @@
 // Se importa en modulo Events, el nombre comienza con mayusculas porque estamos hablando de una clase
 const EventEmitter = require("events");
 
-//nueva instancia de Eventemitter
-const batiSeñal = new EventEmitter();
+//nueva instancia de Eventemitter(emisor del evento)
+const emisorBatiSeñal = new EventEmitter();
 
-//Generamos el evento "crimen"
-batiSeñal.on("crimen", () => {
-  return console.log(`PIM PAM PUM CRASH
-            SOY BATMAN!!!`);
+//Generamos el evento "crimen" y la funcion que va a ocurrir en ese evento(Event Handler)
+emisorBatiSeñal.on("crimen", (enemigo) => {
+  return console.log(`
+            PIM! PAM! PUM! CRASH!
+            SOY BATMAN!!!
+            ${enemigo} abatido`);
 });
 
 //Emitimos en el evento "crimen"
-batiSeñal.emit("crimen");
+emisorBatiSeñal.emit("crimen", "Guason");
